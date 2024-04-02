7c41da8639f54705ac4a3d8ab941c86f3aa62212
CaspeR <konstantynenko.yaroslav@gmail.com>
2024-04-02 22:17:33 +0300

    first commit

---
+++ TestProject/Helpers/AppConfig.swift
@@ -0,0 +1,22 @@
+//
+//  AppConfig.swift
+//  TestProject
+//
+//  Created by Yaroslav Konstantynenko on 02.04.2024.
+//
+
+import UIKit
+
+enum AppConfig {
+    
+    struct Colors {
+        static let backgroundColor = UIColor.black
+        static let backgroundSearchButtonColor = UIColor(named: "violetColor")
+    }
+    
+    struct Image {
+        static let mainImage = UIImage(named: "MainImage")
+        static let indicator = UIImage(named: "Indicator")
+        static let arrow = UIImage(named: "Arrow")
+    }
+}
